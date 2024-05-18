ffa4c94a1e3257c604448f72c8abf0a02f087273
ruthvic_s <rsivakumar@ucsd.edu>
2024-05-18 15:43:33 -0700

    finished creating div to display 4 noodles, need to combine with webpage framework

--- fortunetelling/src/js/noodleOptions.js
+++ fortunetelling/src/js/noodleOptions.js
@@ -5,16 +5,22 @@ function init() {
     let surveyResults = localStorage.getItem('surveyResults')
     console.log("opening noodleOptions with " + surveyResults);
 
-    for (let i = 0; i < 4; i++){
-        let currentNoodle = surveyResults[3];
-        console.log(currentNoodle);
+    let surveyResultsJSON = JSON.parse(surveyResults);
 
-        //document.getElementsByTagName("noodleGridContainer")[i].src = currentNoodle.
-        //let elem1 = document.getElementsByTagName("noodleGridContainer")[0].id;
-        
-    }
+    let noodleImg1 = document.createElement("img");
+    let noodleImg2 = document.createElement("img");
+    let noodleImg3 = document.createElement("img");
+    let noodleImg4 = document.createElement("img");
 
-    //let elem1 = document.getElementsByTagName("grid-item1").id;
+    noodleImg1.src = surveyResultsJSON[0].path
+    noodleImg2.src = surveyResultsJSON[1].path
+    noodleImg3.src = surveyResultsJSON[2].path
+    noodleImg4.src = surveyResultsJSON[3].path
+
+    document.getElementById("grid-item1").appendChild(noodleImg1);
+    document.getElementById("grid-item2").appendChild(noodleImg2);
+    document.getElementById("grid-item3").appendChild(noodleImg3);
+    document.getElementById("grid-item4").appendChild(noodleImg4);
 
 
 }
