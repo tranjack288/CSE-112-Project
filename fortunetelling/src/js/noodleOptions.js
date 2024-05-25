a66d487e54d3f37894b22cea4d30f6de75de38fc
GonzaloAllenPerez444 <GonzaloAllenPerez444@gmail.com>
2024-05-24 17:35:51 -0700

    confirm button completed, noodleOptions should be done

--- fortunetelling/src/js/noodleOptions.js
+++ fortunetelling/src/js/noodleOptions.js
@@ -34,52 +34,62 @@ function init() {
     noodleElement4.appendChild(noodleImg4);
     noodleElement4.querySelector("h3").textContent = surveyResultsJSON[3].noodleName;
 
-    //this initializes first box to be pre-selected - the function returns true if a box is currently selected
+    //this initializes first box to be pre-selected - the function returns [true,noodle object] if a box is currently selected
     let noodleSelected = changeSelected(noodleElement1);
 
 
-    noodleElement1.addEventListener("click", () => {//we'd pass these in local storage to ingredients page 
-        console.log("noodleElement1 was clicked, Noodle: " + JSON.stringify(surveyResultsJSON[0]));
+    noodleElement1.addEventListener("click", () => { //we'd pass these in local storage to ingredients page 
+        
         noodleSelected = changeSelected(noodleElement1);})
 
     noodleElement2.addEventListener("click", () =>{
-         console.log("noodle element2 was clicked, Noodle: " + JSON.stringify(surveyResultsJSON[1]));
+         
          noodleSelected = changeSelected(noodleElement2);});
 
     noodleElement3.addEventListener("click", () => {
-         console.log("noodleelement 3 was clicked, Noodle: " + JSON.stringify(surveyResultsJSON[2]));
+         
          noodleSelected = changeSelected(noodleElement3);});
 
     noodleElement4.addEventListener("click", () => {
-         console.log("noodle element 4 was clicked, Noodle: " + JSON.stringify(surveyResultsJSON[3]));
+         
          noodleSelected = changeSelected(noodleElement4);});
 
-function changeSelected(gridItem){
+     //this makes it so the confirm button will only save chosen noodle to storage and switch pages if a noodle is currently selected
+     const submitOption = document.getElementById('confirmOption');
+     submitOption.addEventListener("click", ()=>{
+          if (!noodleSelected[0]) {alert("you must select a noodle before confirming!")}
+          else {
+               const confirmedGridItem = noodleSelected[1];
+               
+               const confirmedNoodle = surveyResultsJSON[confirmedGridItem.dataset.value -1]
+               
+               console.log("Noodle selected is " + JSON.stringify(confirmedNoodle));
+               localStorage.setItem("noodleChosen",JSON.stringify(confirmedNoodle));
+          }
+     });
+
+     //this function takes in a grid item, checks if/which box is selected, and selects/unselects boxes accordingly
+     function changeSelected(gridItem){
      if (typeof changeSelected.currentlySelected === 'undefined') {
           // Initialize the first square to be pre-selected on init
           //or change currentlySelected to just clicked one if no square is currently clicked
           changeSelected.currentlySelected = gridItem;
           gridItem.style.backgroundColor = '#bb1212';
-          console.log("blank, selecting " + gridItem)
-          return true;}
+          
+          return [true,gridItem];}
      else if (gridItem == changeSelected.currentlySelected){
           //if item is reclicked, set grid back to all unselected.
           changeSelected.currentlySelected = undefined;
           gridItem.style.backgroundColor = 'white';
-          console.log("unselecting" + gridItem);
-     return false;}
+
+     return [false,gridItem];}
      else { //this means one box is currently selected and we click on another box
           changeSelected.currentlySelected.style.backgroundColor = 'white';
           changeSelected.currentlySelected = gridItem;
           gridItem.style.backgroundColor = "#bb1212";
-          console.log("switching to " + gridItem)
-          return true;}
+          return [true,gridItem];}
           
 }
-
-
-
-    //this makes it so the confirm button will only save chosen noodle to storage and switch pages if a noodle is currently selected
     
 
 }
